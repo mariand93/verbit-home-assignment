e373a1c93b35def09f23df45b314bfa58dde8755
dobrmar <marian.dobre@syneto.eu>
2025-04-16 13:31:06 +0300

    init npm, install playwright and add project structure

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'https://example.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+});
---
+++ utils/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    apiBaseUrl: 'https://api.github.com/repos/microsoft/vscode/issues',
+    uiBaseUrl: 'https://github.com/microsoft/vscode',
+  };
+  
